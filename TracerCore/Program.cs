05e3b32f2290690261cd30e877ae2a9003d80ec4
maxiemar <denishicmihail@gmail.com>
2021-09-16 00:56:22 +0300

    Init commit

---
+++ TracerCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TracerCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
