289329cc81970fd01aec626894ab7e95e5dc0a7b
maxiemar <denishicmihail@gmail.com>
2021-09-25 12:08:17 +0300

    refactor: edit docs

--- Tracer.Core/ITracer.cs
+++ Tracer.Core/ITracer.cs
@@ -6,20 +6,20 @@
         {
             /// <summary>
             /// Starts tracing the method in which this <see cref="StartTrace"/> method is called.
-            /// Tracking is performed within the current thread.
+            /// <remarks>Tracking is performed within the current thread.</remarks>
             /// </summary>
             void StartTrace();
 
             /// <summary>
             /// Finishes tracing the method in which this <see cref="StopTrace"/> method is called.
-            /// Tracking is performed within the current thread.
+            /// <remarks>Tracking is performed within the current thread.</remarks>
             /// </summary>
             void StopTrace();
 
             /// <summary>
             /// Return the results of tracing the method in which <see cref="StartTrace"/>
             /// and <see cref="StopTrace"/> methods where called.
-            /// In the case of multithreading use, the results are returned for each thread separately.
+            /// <remarks>In the case of multithreading use, the results are returned for each thread separately.</remarks>
             /// <returns>Tracing results for each thread used in tracing.</returns>
             /// </summary>
             TraceResult GetTraceResult();
